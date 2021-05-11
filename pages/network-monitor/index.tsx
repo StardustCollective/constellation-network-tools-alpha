b48599a1da759c0068bd9a83afe969b2626499aa
toparc077 <toparc077@gmail.com>
2021-05-11 17:32:21 +0800

    chore: init commit

---
+++ pages/network-monitor/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Layout } from "@components/common";
+
+import styles from "./index.module.scss";
+
+const NetworkMonitor: React.FC = () => {
+  return <Layout>This is Network Monitor Page</Layout>;
+};
+
+export default NetworkMonitor;
